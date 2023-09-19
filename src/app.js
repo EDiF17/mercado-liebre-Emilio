443749114368b5bf1670708e008d046635a63333
Emilio Di Fulvio <emiidf1@gmail.com>
2023-09-19 20:25:03 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const port = 3000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
@@ -17,6 +16,8 @@ app.get('/register', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'views/register.html'))
 })
 
+// const port = process.env.PORT || 3030; 
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`Servidor iniciado en http://localhost:${port}`);
 });
