c79237fd3e1ef15df217c9d8bdda988ccbe99092
Emilio Di Fulvio <emiidf1@gmail.com>
2023-09-23 17:39:26 -0300

    arreglos y detalles para finalizacion

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,14 +1,14 @@
 
-    function enviarFormulario() {
-        let nombre = document.querySelector("input[name='nombre']").value;
-        let email = document.querySelector("input[name='email']").value;
+    // function enviarFormulario() {
+    //     let nombre = document.querySelector("input[name='nombre']").value;
+    //     let email = document.querySelector("input[name='email']").value;
 
-        if (nombre === "" || email === "") {
-            alert("Debes rellenar todos los campos obligatorios.");
-            return;
-        }
+    //     if (nombre === "" || email === "") {
+    //         alert("Debes rellenar todos los campos obligatorios.");
+    //         return;
+    //     }
 
-        window.location.href = "/";
-    }
+    //     window.location.href = "/";
+    // }
 
-    document.querySelector("input[type='submit']").addEventListener("click", enviarFormulario);
+    // document.querySelector("input[type='submit']").addEventListener("click", enviarFormulario);
