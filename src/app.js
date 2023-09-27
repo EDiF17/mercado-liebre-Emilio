6916fcfc7433b5a7c2a8287a7ae446ad0620c26c
EDiF17 <emiidf1@gmail.com>
2023-09-27 19:52:33 -0300

    aplicacion de sistema de controllers y ruteo

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
+
+const mainRoutes = require('./routes/main');
+const userRouter = require('./routes/user');
+
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/home.html'));
-});
+app.use('/', mainRoutes); 
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/login.html'));
-}); 
+app.get('/login', userRouter);
 
-app.get('/register', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'views/register.html'))
-})
+app.get('/register', userRouter);
 
 // const port = process.env.PORT || 3030; 
 const port = process.env.PORT || 3000;
